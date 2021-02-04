164c3e5b56bd6c137c201b4fccbfd528bc2ada8d
Nabeel Hayat <nabeel.hayat87@gmail.com>
2021-02-04 06:12:21 +0500

    setup configuration file for default environment variables.

--- server/src/config/index.js
+++ server/src/config/index.js
@@ -1,3 +1,13 @@
-export default {
-  PORT: process.env.PORT || 3000,
-};
+/* eslint-disable import/no-extraneous-dependencies */
+// requires
+import _ from 'lodash';
+
+// module variables
+import config from './config.json';
+
+const defaultConfig = config.development;
+const environment = process.env.NODE_ENV || 'development';
+const environmentConfig = config[environment];
+const finalConfig = _.merge(defaultConfig, environmentConfig);
+
+export default finalConfig;
